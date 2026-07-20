d6f57b1f2349e3f410d1a91585a8ae3ffe066bee
Kostiantyn <tsvetkov.kostiantyn@stud.onu.edu.ua>
2026-07-20 17:30:25 +0300

    Add conversion methods

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,14 +1,27 @@
 package app;
 
-public class Main
-{
+public class Main {
 
-    public static void main(String[] args)
-    {
-        double kg = 10;
-        double pounds = kg * 2.20462;
+    private static final double CONV_K = 2.20462;
 
-        System.out.println("Kilograms: " + kg);
-        System.out.println("Pounds: " + pounds);
+    public static void main(String[] args) {
+        System.out.println("Converter App.");
+
+        double kgs = 5;
+        double pnds = 12;
+
+        double pounds = convKgsToPounds(kgs);
+        double kilos = convPoundsToKgs(pnds);
+
+        System.out.println("Result is " + pounds + " pounds and "
+                + kilos + " kgs.");
+    }
+
+    private static double convKgsToPounds(double kgs) {
+        return kgs * CONV_K;
+    }
+
+    private static double convPoundsToKgs(double pnds) {
+        return pnds / CONV_K;
     }
 }
