b3a648b62e2b05ca43a563255469da7c42e50697
Kostiantyn <tsvetkov.kostiantyn@stud.onu.edu.ua>
2026-07-20 16:44:53 +0300

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,14 @@
+package app;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        double kg = 10;
+        double pounds = kg * 2.20462;
+
+        System.out.println("Kilograms: " + kg);
+        System.out.println("Pounds: " + pounds);
+    }
+}
